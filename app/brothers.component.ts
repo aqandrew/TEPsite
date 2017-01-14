f65e17ee58a839e59ca152decd7a32961f5186bc
dawneraq <dawneraq@gmail.com>
2017-01-14 15:22:25 -0700

    Copy mbostock's Tree of Life for triangly Green Tree

--- app/brothers.component.ts
+++ app/brothers.component.ts
@@ -6,6 +6,7 @@ import { CORE_DIRECTIVES } from '@angular/common';
 
 declare var Papa: any;
 declare var d3: any;
+// declare var Newick: any;
 
 @Component({
 	selector: 'tepei-brothers',
@@ -37,6 +38,13 @@ export class BrothersComponent implements OnInit {
 
 	// data is usable here
 	cleanBrotherData (context, data) {
+		let alphabet = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa',
+			'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega'];
+		let alphabetTau = alphabet.map(letter => 'Tau ' + letter);
+		let alphabetEpsilon = alphabet.map(letter => 'Epsilon ' + letter);
+		var pledgeClasses = ['Founders'].concat(alphabet).concat(alphabetTau).concat(alphabetEpsilon);
+		// console.log(pledgeClasses);
+
 		// Remove depledges.
 		data = data.filter(function (person) { return person.option != 'Depledge'; });
 
@@ -61,6 +69,10 @@ export class BrothersComponent implements OnInit {
 
 				data[b].option = someBro.option;
 			}
+
+			let bigBroPledgeClass = data[b].pledgeClass == 'Founders' ? 0 : pledgeClasses.indexOf(data.find(bro => bro.brotherNumber == data[b].bigBrotherNumber).pledgeClass);
+			data[b].bigDistance = pledgeClasses.indexOf(data[b].pledgeClass) - bigBroPledgeClass;
+			// console.log(`${data[b].firstName} ${data[b].lastName} bigDistance: ${data[b].bigDistance}`);
 		}
 
 		console.log('cleanBrotherData: ', data);
@@ -109,7 +121,7 @@ export class BrothersComponent implements OnInit {
 	writeNewickString(brother) {
 		let hisLittles = this.getLittles(brother);
 		let nodeRepresentation = someBrother => someBrother.brotherNumber + (!someBrother.bigBrotherNumber ? '' : ':' + this.getBigDistance(someBrother));
-		return !hisLittles.length ? nodeRepresentation(brother) : '(' + hisLittles.map(someBrother => this.writeNewickString(someBrother) ).join(',') + ')' + nodeRepresentation(brother);
+		return !hisLittles.length ? nodeRepresentation(brother) : '(' + hisLittles.map(someBrother => this.writeNewickString(someBrother)).join(',') + ')' + nodeRepresentation(brother);
 	}
 
 	getLittles(brother) {
@@ -120,6 +132,7 @@ export class BrothersComponent implements OnInit {
 		var self = this;
 		var h = 600;
 		var rectHeight = 50;
+		var svgWidth = this.phyloTrees.length * (this.rectWidth + this.rectSpacingX) + this.rectSpacingX;
 
 		var el = document.getElementById('brother-board');
 		var divStyle = window.getComputedStyle(el, null).getPropertyValue('font-size');
@@ -127,74 +140,190 @@ export class BrothersComponent implements OnInit {
 
 		//console.log('data to draw brother boards: ', this.brotherData);
 
-		var svg = d3.select('#brother-board')
-			.append('svg')
-				.attr('width', 1600);
+		// Copyright 2016 Mike Bostock https://d3js.org
+		var outerRadius = 960 / 2,
+    		innerRadius = outerRadius - 170;
 
-		var background = svg.append('rect')
-			.attr('height', 2500) // TODO temporary values
-			.attr('width', 1600)
-			.style('fill', '#dde2eb');
+		var color = d3.scaleOrdinal()
+			.domain(["Bacteria", "Eukaryota", "Archaea"])
+			.range(d3.schemeCategory10);
 
-		var nodes = svg.selectAll('g.node')
-			.data(self.brotherData)
-			.enter()
-				.append('g');
+		var cluster = d3.cluster()
+			.size([360, innerRadius])
+			.separation(function(a, b) { return 1; });
 
-		var rectangles = nodes.append('rect')
-			.attr('x', function (brother) {
-				var x0 = self.getHorizOffset(brother);
-				brother.x0 = x0;
-				return x0;
-			})
-			.attr('y', function (brother) {
-				var y0 = rectHeight * (self.getPledgeClassHeight(brother) /*+ self.getBigDistance(brother)*/) + self.rectSpacingY;
-				brother.y0 = y0;
-				return y0;
-			})
-			.attr('width', self.rectWidth)
-			.attr('height', 20)
-			.attr('class', function (brother) { return brother.option.toLowerCase(); });
+		var svg = d3.select("#brother-board").append("svg")
+			.attr("width", outerRadius * 2)
+			.attr("height", outerRadius * 2);
+
+		// Copyright 2011 Jason Davies https://github.com/jasondavies/newick.js
+		function parseNewick(a){for(var e=[],r={},s=a.split(/\s*(;|\(|\)|,|:)\s*/),t=0;t<s.length;t++){var n=s[t];switch(n){case"(":var c={};r.branchset=[c],e.push(r),r=c;break;case",":var c={};e[e.length-1].branchset.push(c),r=c;break;case")":r=e.pop();break;case":":break;default:var h=s[t-1];")"==h||"("==h||","==h?r.name=n:":"==h&&(r.length=parseFloat(n))}}return r}
+
+		var chart = svg.append("g")
+			.attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
+
+		// TODO test using The Best Tree
+		var sampleTree = this.brotherData.filter(brother => brother.option == 'Green');
+		var life = this.writeNewickString(sampleTree[0]);
+		console.log(life);
+		var root = d3.hierarchy(parseNewick(life), function(d) { return d.branchset; })
+			.sum(function(d) { return d.branchset ? 0 : 1; })
+			.sort(function(a, b) { return (a.value - b.value) || d3.ascending(a.data.length, b.data.length); });
+      	console.log(root);
+
+  		cluster(root);
+
+		var input = d3.select("#show-length input").on("change", changed),
+			timeout = setTimeout(function() { input.property("checked", true).each(changed); }, 2000);
+
+		setRadius(root, root.data.length = 0, innerRadius / maxLength(root));
+		setColor(root);
+
+		var linkExtension = chart.append("g")
+			.attr("class", "link-extensions")
+			.selectAll("path")
+			.data(root.links().filter(function(d) { return !d.target.children; }))
+			.enter().append("path")
+				.each(function(d) { d.target.linkExtensionNode = this; })
+				.attr("d", linkExtensionConstant);
+
+		var link = chart.append("g")
+			.attr("class", "links")
+			.selectAll("path")
+			.data(root.links())
+			.enter().append("path")
+				.each(function(d) { d.target.linkNode = this; })
+				.attr("d", linkConstant)
+				.attr("stroke", function(d) { return d.target.color; });
+
+		chart.append("g")
+			.attr("class", "labels")
+			.selectAll("text")
+			.data(root.leaves())
+			.enter().append("text")
+				.attr("dy", ".31em")
+				.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (innerRadius + 4) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
+				.attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+				.text(function(d) { let bro = self.getBrotherById(d.data.name); return bro.firstName + ' ' + bro.lastName; })
+				.on("mouseover", mouseovered(true))
+				.on("mouseout", mouseovered(false));
+
+		function changed() {
+			clearTimeout(timeout);
+			var t = d3.transition().duration(750);
+			linkExtension.transition(t).attr("d", this.checked ? linkExtensionVariable : linkExtensionConstant);
+			link.transition(t).attr("d", this.checked ? linkVariable : linkConstant);
+		}
+
+		function mouseovered(active) {
+			return function(d) {
+				d3.select(this).classed("label--active", active);
+				d3.select(d.linkExtensionNode).classed("link-extension--active", active).each(moveToFront);
+				do d3.select(d.linkNode).classed("link--active", active).each(moveToFront); while (d = d.parent);
+			};
+		}
+
+		function moveToFront() {
+			this.parentNode.appendChild(this);
+		}
+
+		// Compute the maximum cumulative length of any node in the tree.
+		function maxLength(d) {
+			return d.data.length + (d.children ? d3.max(d.children, maxLength) : 0);
+		}
+
+		// Set the radius of each node by recursively summing and scaling the distance from the root.
+		function setRadius(d, y0, k) {
+			d.radius = (y0 += d.data.length) * k;
+			if (d.children) d.children.forEach(function(d) { setRadius(d, y0, k); });
+		}
+
+		// Set the color of each node by recursively inheriting.
+		function setColor(d) {
+			var name = d.data.name;
+			d.color = color.domain().indexOf(name) >= 0 ? color(name) : d.parent ? d.parent.color : null;
+			if (d.children) d.children.forEach(setColor);
+		}
+
+		function linkVariable(d) {
+			return linkStep(d.source.x, d.source.radius, d.target.x, d.target.radius);
+		}
+
+		function linkConstant(d) {
+			return linkStep(d.source.x, d.source.y, d.target.x, d.target.y);
+		}
+
+		function linkExtensionVariable(d) {
+			return linkStep(d.target.x, d.target.radius, d.target.x, innerRadius);
+		}
+
+		function linkExtensionConstant(d) {
+			return linkStep(d.target.x, d.target.y, d.target.x, innerRadius);
+		}
+
+		// Like d3.svg.diagonal.radial, but with square corners.
+		function linkStep(startAngle, startRadius, endAngle, endRadius) {
+		var c0 = Math.cos(startAngle = (startAngle - 90) / 180 * Math.PI),
+			s0 = Math.sin(startAngle),
+			c1 = Math.cos(endAngle = (endAngle - 90) / 180 * Math.PI),
+			s1 = Math.sin(endAngle);
+		return "M" + startRadius * c0 + "," + startRadius * s0
+			+ (endAngle === startAngle ? "" : "A" + startRadius + "," + startRadius + " 0 0 " + (endAngle > startAngle ? 1 : 0) + " " + startRadius * c1 + "," + startRadius * s1)
+			+ "L" + endRadius * c1 + "," + endRadius * s1;
+		}
+
+		// var nodes = svg.selectAll('g.node')
+		// 	.data(self.brotherData)
+		// 	.enter()
+		// 		.append('g');
+
+		// var rectangles = nodes.append('rect')
+		// 	.attr('x', function (brother) {
+		// 		var x0 = self.getHorizOffset(brother);
+		// 		brother.x0 = x0;
+		// 		return x0;
+		// 	})
+		// 	.attr('y', function (brother) {
+		// 		var y0 = rectHeight * (self.getPledgeClassHeight(brother) /*+ self.getBigDistance(brother)*/) + self.rectSpacingY;
+		// 		brother.y0 = y0;
+		// 		return y0;
+		// 	})
+		// 	.attr('width', self.rectWidth)
+		// 	.attr('height', 20)
+		// 	.attr('class', function (brother) { return brother.option.toLowerCase(); });
 		
-		var links = svg.selectAll('path.link')
-			.data(self.brotherData)
-			.enter()
-				.insert('path', 'g')
-				.attr('class', 'link')
-				.attr('d', function (brother) {
-					if (brother.pledgeClass != 'Founders') {
-						var bigBrother = self.getBigBrother(brother);
-						return 'M' + brother.x0 + ',' + brother.y0 +
-							'C' + (brother.x0 + bigBrother.x0) / 2 + ',' + brother.y0 +
-							' ' + (brother.x0 + bigBrother.x0) / 2 + ',' + bigBrother.y0 +
-							' ' + bigBrother.x0 + ',' + bigBrother.y0;
-					}
-				})
-				.attr('transform', 'translate(' + self.rectWidth / 2 + ')'); // centered with respect to each rectangle
+		// var links = svg.selectAll('path.link')
+		// 	.data(self.brotherData)
+		// 	.enter()
+		// 		.insert('path', 'g')
+		// 		.attr('class', 'link')
+		// 		.attr('d', function (brother) {
+		// 			if (brother.pledgeClass != 'Founders') {
+		// 				var bigBrother = self.getBigBrother(brother);
+		// 				return 'M' + brother.x0 + ',' + brother.y0 +
+		// 					'C' + (brother.x0 + bigBrother.x0) / 2 + ',' + brother.y0 +
+		// 					' ' + (brother.x0 + bigBrother.x0) / 2 + ',' + bigBrother.y0 +
+		// 					' ' + bigBrother.x0 + ',' + bigBrother.y0;
+		// 			}
+		// 		})
+		// 		.attr('transform', 'translate(' + self.rectWidth / 2 + ')'); // centered with respect to each rectangle
 
-		var names = nodes.append('text')
-			.text(function (brother) {
-				// TODO add pledge names
-				return brother.firstName + ' ' + brother.lastName;
-			})
-			.attr('x', function (brother) {
-				return self.getHorizOffset(brother) + self.rectWidth / 2; // to center text in rectangle
-				})
-			.attr('y', function (brother) {
-				return rectHeight * self.getPledgeClassHeight(brother) + self.rectSpacingY + fontSize;
-			})
-			.attr('text-anchor', 'middle')
-			.attr('class', function (brother) {
-				var isSilver = brother.option == 'Black';
-				return isSilver ? 'silver-text' : 'black';
-			});
-
-		// Matthew Nelson, 336 should be 2
-		//debugger;
-		// for (let i = 0; i < self.brotherData.length; i++) {
-		// 	let testBrother = self.brotherData[i]; // Dan Flaherty, 328, should be 1
-		// 	console.log(testBrother.firstName + ' ' + testBrother.lastName + ' bigDistance: ' + self.getBigDistance(testBrother));
-		// }
+		// var names = nodes.append('text')
+		// 	.text(function (brother) {
+		// 		// TODO add pledge names
+		// 		return brother.firstName + ' ' + brother.lastName;
+		// 	})
+		// 	.attr('x', function (brother) {
+		// 		return self.getHorizOffset(brother) + self.rectWidth / 2; // to center text in rectangle
+		// 		})
+		// 	.attr('y', function (brother) {
+		// 		return rectHeight * self.getPledgeClassHeight(brother) + self.rectSpacingY + fontSize;
+		// 	})
+		// 	.attr('text-anchor', 'middle')
+		// 	.attr('class', function (brother) {
+		// 		var isSilver = brother.option == 'Black';
+		// 		return isSilver ? 'silver-text' : 'black';
+		// 	});
 	}
 
 	getBrotherById (id) {
